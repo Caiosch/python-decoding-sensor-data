185cf2df480c3372ce43750d2c8faa9c08fbdfeb
Caio Schmid <caioschmid@gmail.com>
2020-08-16 04:30:16 -0300

    Module 3 done

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,6 +1,7 @@
 from load_data import load_sensor_data
 from house_info import HouseInfo
 from datetime import date, datetime
+from temperature_info import TemperatureData
 
 
 data = []
@@ -11,8 +12,17 @@ print("\nLoaded records: {}".format(len(data)))
 
 house_info = HouseInfo(data)
 test_area = 1
-recs = house_info.get_data_by_area('id', rec_area = test_area)
+recs = house_info.get_data_by_area('id', rec_area=test_area)
 print('\nHouse sensor records from area {} = {}'.format(test_area, len(recs)))
 test_date = datetime.strptime('5/9/20', '%m/%d/%y')
-recs = house_info.get_data_by_date('id', rec_date = test_date)
-print('\nHouse sensor records for date {} = {}'.format(test_date.strftime('%m/%d/%y'), len(recs)))
+recs = house_info.get_data_by_date('id', rec_date=test_date)
+print('\nHouse sensor records for date {} = {}'.format(
+    test_date.strftime('%m/%d/%y'), len(recs)))
+
+temperature_data = TemperatureData(data)
+recs = temperature_data.get_data_by_area(rec_area=test_area)
+print('\nHouse Temperature sensor record for area {} = {}'.format(test_area, len(recs)))
+print('\nMaximum: {0}, Minimum: {1} temperatures'.format(max(recs), min(recs)))
+recs = temperature_data.get_data_by_date(rec_date=test_date)
+print('\nHouse Temperature sensor records for date: {} = {}'.format(test_date.strftime('%m/%d/%y'), len(recs)))
+print('\nMaximum: {0}, Minimum: {1} temperature'.format(max(recs), min(recs)))
